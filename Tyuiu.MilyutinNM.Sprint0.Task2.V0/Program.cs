0b5492e8782aaeefc6a9990c1112e2cf3f231639
Tyuiu.milyutin@outlook.com <mm1llnikita@gmail.com>
2024-09-14 20:22:48 +0500

    Task2

---
+++ Tyuiu.MilyutinNM.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.MilyutinNM.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.MilyutinNM.Sprint0.Task2.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Игорь"));
+            Console.ReadKey();
+            Console.ReadKey();
+        }
+    }
+}
